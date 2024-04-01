a716cb54ce99c609c6b12388999007ede8e616fc
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-01 20:37:27 +0200

    new changes

--- DBconfig/Dbconnection.js
+++ DBconfig/Dbconnection.js
@@ -1,10 +1,9 @@
-const { error } = require('console');
-const { string } = require('joi');
-const   mongoose = require('mongoose');
-const { type } = require('os');
-const userSchema=   mongoose.connect('mongodb://localhost:27017/Blogsite')
-.then(console.log('the connection  to mongodb is set... '))
-.catch((error)=>{
-    console.error('error while stting connection to mongdb')
-});
+const mongoose = require("mongoose");
+const dbconn = mongoose
+  .connect("mongodb://localhost:27017/Blogsite")
+  .then(console.log("the connection  to mongodb is set... "))
+  .catch((error) => {
+    console.error("error while stting connection to mongdb");
+  });
 
+  module.exports.dbconn;
--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const dbconn = require("./DBconfig/Dbconnection");
 const usermodel= require('./models/userschema');
+const bodyparser =require('body-parser');
+const path =require('path');
+const fs= require('fs');
 require("dotenv").config();
 const app = express();
 PORT = process.env.PORT || 4000;
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+
 
 app.listen(PORT, () => console.log(`the server is running on ${PORT} ...`));
