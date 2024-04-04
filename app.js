2520bac6c5bbc3b4190af3becc376967f505bd6e
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-04 09:02:58 +0200

    new changes

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const express = require("express");
 const dbconn = require("./DBconfig/Dbconnection");
 const usermodel = require("./models/userschema");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const ejs = require("ejs");
 const Joi = require("joi");
-const BlogModel = require("./models/blogschema");
+const models = require("./models/blogschema");
 const path = require("path");
+const { model } = require("mongoose");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
@@ -82,32 +90,72 @@ app.post("/login", async (req, res) => {
 app.get("/createblog", (req, res) => {
   res.render("createblog");
 });
+
 app.post("/createblog", async (req, res) => {
-
-  const blogvalidateschema= joi.object({
-    authorname:Joi.string().min(5).required(),
-    blogtitle:Joi.string().min(5).required(),
-    blogdescription:Joi.string().min(5).required(),
-    blogcontent:Joi.string().min(5).required(),
-    image:Joi.string().min(5).required(),
-
-  })
-  const { authorname, blogtitle, blogdes, blogcontent, image } = req.body;
-  
-  const storeblog = new BlogModel({
-    authorname,
-    blogtitle,
-    blogdescription: blogdes,
-    blogcontent,
-    image,
-  });
-
-
   try {
-    const saveblog = await storeblog.save();
-    res.send("blog published successfully");
+    const blogvalidateschema = Joi.object({
+      authorname: Joi.string().required(),
+      blogtitle: Joi.string().min(5).required(),
+      blogdes: Joi.string().min(5).required(),
+      blogcontent: Joi.string().min(5).required(),
+      image: Joi.string().min(5),
+    });
+
+    const validateblog = blogvalidateschema.validate(req.body);
+    if (validateblog.error) {
+     
+      return res.status(400).send(validateblog.error.details[0].message);
+    }
+
+    const { authorname, blogtitle, blogdes, blogcontent } = req.body;
+
+    const storeblog = new models.BlogModel({
+      authorname,
+      blogtitle,
+      blogdescription: blogdes,
+      blogcontent
+    
+    });
+    try {
+      console.log(req.body);
+      await storeblog.save();
+      res.send("Blog data received successfully");
+    } catch (error) {
+      res.status(400).send("failed to save blog post");
+    }
   } catch (error) {
-    res.status(401).send("error in publication ", error.message);
+    console.error("Error creating blog:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/authordesc", async (req, res) => {
+  try {
+    const authorDescschema = Joi.object({
+      location: Joi.string().min(3).required(),
+      github: Joi.string().min(5).required().default("no git hub account"),
+      academic: Joi.string().required(),
+    });
+
+    let result = authorDescschema.validate(req.body);
+    if (result.error) {
+      console.log(req.body);
+      return res.status(400).send(authorDescschema.error.details[0].message);
+    }
+
+    const { location, github, academic } = req.body;
+    const authors = new models.authorModel({
+      location,
+      github,
+      academic,
+    });
+    console.log(req.body);
+    await authors.save();
+
+    res.send("Blog published successfully with author details");
+  } catch (error) {
+    console.error("Error adding author details:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
--- models/blogschema.js
+++ models/blogschema.js
@@ -1,13 +1,12 @@
-const { string } = require("joi");
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-  blog: {
+  
     authorname: {
       type: String,
       required: true,
       minlength: 3,
-      maxlength: 50,
+      maxlength: 30,
     },
     blogtitle: {
       type: String,
@@ -19,30 +18,36 @@ const blogSchema = new mongoose.Schema({
       type: String,
       required: true,
       minlength: 10,
-      maxlength: 200,
+      maxlength: 400,
     },
     blogcontent: {
       type: String,
       required: true,
       minlength: 50,
     },
-    image: {
-      type: String, 
-      required: true
-    },
+    // image: {
+    //   type: String,
+    //   required: true,
+    // },
     date: {
       type: Date,
       default: Date.now,
-    }   
-  },
-  authordetails: {
-    location: { type:String, minlength:5, required: true },
-    github: { type: String, trim: true },
-    academic:{type:String,required:true}
+    },
 
-  }
 });
 
+const authorschema = mongoose.Schema({
+  location: { type: String, minlength: 3, required: true },
+  github: { type: String, trim: true },
+  academic: { type: String, required: true },
+});
+
+const authorModel = new mongoose.model("Author", authorschema);
 const BlogModel = mongoose.model("Blog", blogSchema);
 
-module.exports = BlogModel;
+
+module.exports = {
+  BlogModel,
+  authorModel
+};
+
