d6f37a216d53b7f9bce454be9f0ab47a00cba4f4
Ashrafu <ashraftuyubahe001@gmail.com>
2024-04-30 21:46:42 +0200

    fetching blogs  from database

--- React_front_end/blogsite/src/App.js
+++ React_front_end/blogsite/src/App.js
@@ -1,36 +1,34 @@
-import {react, useEffect, useState} from 'react';
-
-const [blogs,setblogs]=useState([]);
-
-
-useEffect({
-    fetchBlogs: () => {
-    fetch("localhost:4000/blogs")
-      .then((response) => {
-        response.json();
-
-      })
-      .then((blogs) => {
-        setblogs(blogs);
-        console.log(blogs);
-
-      });
-
-  }
-})
-
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/blogs") // Note: you missed "http://" in the URL
+      .then((response) => response.json())
+      .then((blogs) => {
+        setBlogs(blogs);
+        console.log(blogs);
+      });
+  }, []);
+
   return (
-   <>
-  <div className='app'>
-     {blogs.map((blog)=>{
-      return 
-      
-     })}
-  </div>
-   
-   </>
+    <div className="app">
+      {blogs.map((blog) => {
+        return (
+          <div key={blog._id} className="blog" data-blogid={blog._id}>
+            <h3 className="blogtitle">
+              <a href={`displayblog?id=${blog._id}`}>
+                {blog.data.blogtitle}
+              </a>
+            </h3>
+            <h5>{blog.data.blogdescription}</h5>
+            <p>written by: {blog.data.authorname}</p>
+            <p> written on {blog.data.date}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
