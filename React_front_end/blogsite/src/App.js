e164de3bcd3ad20e98b7ae1e2b2467fbc4f1e93c
Ashrafu <ashraftuyubahe001@gmail.com>
2024-05-01 08:02:11 +0200

    finished conection two parts

--- React_front_end/blogsite/src/App.js
+++ React_front_end/blogsite/src/App.js
@@ -1,43 +1,44 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-
-
 
 function App() {
   const [blogs, setBlogs] = useState([]);
-  
-axios.defaults.baseURL = "http://127.0.0.1:4000";
-axios.defaults.withCredentials = true;
 
-useEffect(() => {
-  axios
-    .get("http://localhost:4000")
-    .then((response) => {
-      console.log(response.data);
-      setBlogs(response.data);
+  useEffect(() => {
+    fetch("http://localhost:4000/blogs", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // Optionally, you can include credentials if needed
+        credentials: "include",
+      },
     })
-    .catch((error) => {
-      console.log(error);
-    });
-}, []);
-
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <div className="app">
-      {blogs.map((blog) => {
-        return (
-          <div key={blog._id} className="blog" data-blogid={blog._id}>
-            <h3 className="blogtitle">
-              <a href={`displayblog?id=${blog._id}`}>
-                {blog.data.blogtitle}
-              </a>
-            </h3>
-            <h5>{blog.data.blogdescription}</h5>
-            <p>written by: {blog.data.authorname}</p>
-            <p> written on {blog.data.date}</p>
-          </div>
-        );
-      })}
+      {blogs.map((blog) => (
+        <div key={blog._id} className="blog" data-blogid={blog._id}>
+          <h3 className="blogtitle">
+            <a href={`displayblog?id=${blog._id}`}>{blog.data.blogtitle}</a>
+          </h3>
+          <h5>{blog.data.blogdescription}</h5>
+          <p>written by: {blog.data.authorname}</p>
+          <p> written on {blog.data.date}</p>
+        </div>
+      ))}
     </div>
   );
 }
