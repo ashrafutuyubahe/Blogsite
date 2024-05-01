e6243c3a3f0aa3b50ddf1ea44261a9e43e5eb9df
Ashrafu <ashraftuyubahe001@gmail.com>
2024-05-01 09:50:25 +0200

    adding registering functionality

--- React_front_end/blogsite/src/App.js
+++ React_front_end/blogsite/src/App.js
@@ -3,6 +3,8 @@ import './app.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DisplayBlog from './components/blogdisplay'
 import NavBar from "./components/navbar";
+import Register from "./pages/register";
+
 
 export default function App() {
   return (
@@ -11,8 +13,8 @@ export default function App() {
      <NavBar />
         {/* <DisplayBlog /> */}
         <Routes>
-          <Route path="/createblog" />
-          <Route path="/register"/>
+          <Route path="/createblog"  />
+          <Route path="/register" element={<Register/>}/>
           <Route path="/login" />
         </Routes>
      </div>
--- React_front_end/blogsite/src/pages/register.js
+++ React_front_end/blogsite/src/pages/register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './register.css'
 
 
 
@@ -8,9 +9,28 @@ export default function Register(){
 
 
 return(
-<div  className="contents">
+<div  className="body">
 
-    
+<div class="container">
+        <h2>Create an Account</h2>
+        <form action="/register" method="post" id="registrationForm">
+            <div>
+          <label for="email">Email:</label>
+                <input type="email" id="email" name="useremail" />
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="userpassword" />
+            </div>
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="username" />
+            </div>
+            <button type="submit"><a>Register</a></button>
+            <h5>already have  an account.<a id="haveAccount" href="/login">Log in</a></h5>
+        </form>
+        
+    </div>
 </div>
 
 
