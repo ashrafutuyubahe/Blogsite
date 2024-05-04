83c4439baf40b6a0b49fd2a18ef8593d46be30dd
Ashrafu <ashraftuyubahe001@gmail.com>
2024-05-04 11:28:08 +0200

    adding new changes

--- React_front_end/blogsite/src/components/blogCard.jsx
+++ React_front_end/blogsite/src/components/blogCard.jsx
@@ -1,13 +1,15 @@
 import React from "react"
- 
+
 
 export default function Card({ blog }) {
+  const {images,blogs}=blog;
   return (
+    
     <div className="blogcard">
-      {blog &&
-        blog.map((blogItem) => (
+      {blogs &&
+        blogs.map((blogItem) => (
           <div key={blogItem._id} className="blog" data-blogid={blogItem._id}>
-
+           <img key={images[0]._id} src={images[0].filename} alt="image" />
             <h3 className="blogtitle">
               <a href={`displayblog?id=${blogItem._id}`}>{blogItem.data.blogtitle}</a>
             </h3>
